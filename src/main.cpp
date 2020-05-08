373bc5564437577799d83c3d936303e266e80469
Evan Savage <evan@savageinter.net>
2020-05-07 20:21:57 -0400

    Thermometer reading from DS18B20
    using the OneWire and DallasTemperature libraries.  Right now we're
    reading this at 11-bit resolution, which gives the temperature in
    increments of 0.125 degrees Celsius.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,85 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <AsyncJson.h>
+#include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <OneWire.h>
 
 #include "private.h"
 
-#define ESP8266_LED 5
+#define PIN_ONE_WIRE 0
+#define PIN_LED 5
+
+#define HTTP_OK 200
+#define HTTP_BAD_REQUEST 400
+#define HTTP_NOT_FOUND 404
+
+#define TEMP_RESOLUTION 11
+#define TEMP_DELAY 750 / (1 << (12 - TEMP_RESOLUTION))
 
 AsyncWebServer server(80);
 
+OneWire oneWire(PIN_ONE_WIRE);
+DallasTemperature thermometerManager(&oneWire);
+
+DeviceAddress thermometer;
+
+uint8_t led = LOW;
+float temp = DEVICE_DISCONNECTED_C;
+unsigned long lastRequestedAt = 0ul;
+
+struct HttpError {
+  uint16_t code;
+  const char* message;
+};
+
+const char index_html[] PROGMEM =
+  "<!DOCTYPE HTML>"
+  "<html><head>"
+    "<meta charset=\"utf-8\">"
+    "<link href=\"http://192.168.0.13:8000/main.css\" rel=\"stylesheet\">"
+  "</head><body>"
+    "<h1>It works with async from PROGMEM!</h1>"
+    "<script src=\"http://192.168.0.13:8000/main.js\"></script>"
+  "</body></html>";
+
 // HARDWARE SETUP
 
-void initHardware() {
-  pinMode(ESP8266_LED, OUTPUT);
-  digitalWrite(ESP8266_LED, LOW);
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+bool initThermometer() {
+  if (!thermometerManager.getAddress(thermometer, 0)) {
+    Serial.println("Could not get thermometer address!");
+    return false;  
+  }
+  Serial.print("Thermometer address: ");
+  printAddress(thermometer);
+  Serial.println();
+
+  if (OneWire::crc8(thermometer, 7) != thermometer[7]) {
+    Serial.println("Invalid address CRC!");
+    return false;
+  }
+
+  thermometerManager.setResolution(thermometer, TEMP_RESOLUTION);
+  thermometerManager.setWaitForConversion(false);
+  return true;
+}
+
+bool initHardware() {
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 
   Serial.begin(115200);
   Serial.println();
+
+  return initThermometer();
 }
 
 // WIFI CONNECTION SETUP
@@ -27,7 +89,7 @@ uint8_t initWifiConnection() {
   WiFi.begin(WIFI_SSID, WIFI_PW);
 
   Serial.print("Connecting");
-  int i = 0;
+  uint8_t i = 0;
   while (true) {
     // check wifi status
     uint8_t status = WiFi.status();
@@ -38,8 +100,8 @@ uint8_t initWifiConnection() {
     }
 
     // blink LED, print to serial
-    int led = i % 2 == 0 ? HIGH : LOW;
-    digitalWrite(ESP8266_LED, led);
+    led = i % 2 == 0 ? HIGH : LOW;
+    digitalWrite(PIN_LED, led);
     delay(100);
     if (i % 5 == 0) {
       Serial.print(".");
@@ -58,7 +120,7 @@ void notifyWifiStatus(uint8_t status) {
     Serial.println("Invalid password!");
   }
 
-  digitalWrite(ESP8266_LED, LOW);
+  digitalWrite(PIN_LED, LOW);
   Serial.println();
 }
 
@@ -69,26 +131,6 @@ uint8_t initWifi() {
 }
 
 // WEB SERVER SETUP
-
-#define HTTP_OK 200
-#define HTTP_BAD_REQUEST 400
-#define HTTP_NOT_FOUND 404
-
-struct HttpError {
-  int code;
-  const char* message;
-};
-
-int led = LOW;
-const char index_html[] PROGMEM =
-  "<!DOCTYPE HTML>"
-  "<html><head>"
-    "<meta charset=\"utf-8\">"
-    "<link href=\"http://192.168.0.13:8000/main.css\" rel=\"stylesheet\">"
-  "</head><body>"
-    "<h1>It works with async from PROGMEM!</h1>"
-    "<script src=\"http://192.168.0.13:8000/main.js\"></script>"
-  "</body></html>";
 
 void sendJsonError(AsyncWebServerRequest* request, const HttpError& error) {
   AsyncJsonResponse* response = new AsyncJsonResponse();
@@ -107,6 +149,7 @@ void sendJsonSettings(AsyncWebServerRequest* request) {
 
   const JsonObject& root = response->getRoot();
   root["led"] = led;
+  root["temp"] = temp;
 
   response->setLength();
   request->send(response);
@@ -133,10 +176,10 @@ void putSettings(AsyncWebServerRequest* request, JsonVariant& json) {
     return;
   }
 
-  int ledNew = root["led"] | -1;
+  uint8_t ledNew = root["led"] | -1;
   if (ledNew == LOW || ledNew == HIGH) {
     led = ledNew;
-    digitalWrite(ESP8266_LED, led);
+    digitalWrite(PIN_LED, led);
   } else {
     HttpError error { HTTP_BAD_REQUEST, "invalid or missing LED value" };
     sendJsonError(request, error);
@@ -163,11 +206,29 @@ void initServer() {
 // MAIN FUNCTIONS
 
 void setup() {
-  initHardware();
+  if (!initHardware()) {
+    return;
+  }
   if (initWifi() != WL_CONNECTED) {
     return;
   }
   initServer();
 }
 
-void loop() {}
+void loop() {
+  if (lastRequestedAt == 0ul) {
+    thermometerManager.requestTemperatures();
+    lastRequestedAt = millis();
+  } else if (millis() - lastRequestedAt > TEMP_DELAY) {
+    float tempNew = thermometerManager.getTempC(thermometer);
+    if (tempNew == DEVICE_DISCONNECTED_C) {
+      Serial.println("Could not read temperature!");
+    } else {
+      temp = tempNew;
+    }
+    thermometerManager.requestTemperatures();
+    lastRequestedAt = millis();
+  }
+  
+  delay(100);
+}
